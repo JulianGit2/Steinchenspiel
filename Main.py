bd137d329566769200dc56741c4ef71df2f5fdee
JulianGit2 <julian_stanger@web.de>
2020-05-10 13:15:14 +0200

    Turtle first try

--- Main.py
+++ Main.py
@@ -1,2 +1,21 @@
 # First draft
 
+import turtle
+
+# Set up the screen
+wn = turtle.Screen()
+wn.setup(1627, 842)
+wn.bgpic("bgpic.gif")
+wn.title("Steichenspiel")
+turtle.penup()
+
+# Set up the board
+turtle.setx(-200)
+turtle.sety(200)
+turtle.write("2", font=("Arial", 24, "normal"))
+
+# Close Screen
+wn.exitonclick()
+
+
+
